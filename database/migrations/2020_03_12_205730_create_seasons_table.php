871a077aadd531ed17965b73757dd11ee731681d
Florian Bernard <cav0n@hotmail.fr>
2020-03-12 22:15:54 +0100

    Added models

---
+++ database/migrations/2020_03_12_205730_create_seasons_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seasons', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+
+            $table->bigInteger('serie_id')->unsigned();
+
+            $table->foreign('serie_id')
+                    ->references('id')->on('series')
+                    ->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seasons');
+    }
+}
