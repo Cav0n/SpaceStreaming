aca56647921606d75e9c326866f6b2ce381818f2
Florian Bernard <cav0n@hotmail.fr>
2020-03-12 19:35:06 +0100

    Added link to episode 1 of space dandy

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,5 +2,5 @@
 
 
 @section('content')
-
+<video src="{{ asset('videos/SDS1E1.mkv') }}" type='video/x-matroska; codecs="theora, vorbis"' autoplay controls onerror="failed(event)" ></video>
 @endsection
