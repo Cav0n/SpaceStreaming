25aa13c9c0361835130986fe72a7299f23566b1d
Florian Bernard <cav0n@hotmail.fr>
2020-03-12 18:59:47 +0100

    Added first template and page

---
+++ resources/views/templates/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Space Streaming</title>
+</head>
+<body>
+    <h1>BIENVENUE SUR SPACE STREAMING</h1>
+
+    @yield('content')
+</body>
+</html>
