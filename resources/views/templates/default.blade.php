c938ee85f5975c048d1a46248a197855dab2d668
Florian Bernard <cav0n@hotmail.fr>
2020-03-12 20:38:21 +0100

    Added JQuery and Bootstrap

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- App CSS --}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
     <title>Space Streaming</title>
 </head>
 <body>
+    <script src="{{asset('js/app.js')}}"></script>
+
     <h1>BIENVENUE SUR SPACE STREAMING</h1>
 
     @yield('content')
