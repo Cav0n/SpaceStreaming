100d9cfc7199e6c8d49841f746784184ba728292
Florian Bernard <cav0n@hotmail.fr>
2020-03-14 01:11:34 +0100

    Fixed new badge

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -17,7 +17,7 @@
                     <a href='{{ route('episode.show', ['episode' => $episode]) }}' class="text-light">
                         Episode {{ $episode->number }}</a>
 
-                    @if($episode->created_at->diffInHours(Carbon\Carbon::now(), false) > 15)
+                    @if($episode->created_at->diffInHours(Carbon\Carbon::now(), false) < 15)
                         <span class="badge badge-light ml-2">Nouveau</span>
                     @endif
                 </li>
